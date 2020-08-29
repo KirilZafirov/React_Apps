ac57250aecbdd72ec0bad1b57325abcba7774c3e
Kiril <kiril_zafirov@yahoo.com>
2020-08-29 23:16:30 +0200

    mongoDB connection

--- contact-keeper/server.js
+++ contact-keeper/server.js
@@ -1,8 +1,8 @@
-
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
-
+connectDB();
 app.get('/', (req , res) => res.json({ msg: 'welcome to the contact keeper API'}));
 
 
