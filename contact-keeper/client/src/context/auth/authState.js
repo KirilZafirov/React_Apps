bd32ff63c783b1b88dceaf4693241b5a64ef45ed
Kiril <kiril_zafirov@yahoo.com>
2020-08-30 16:53:21 +0200

    alert context

--- contact-keeper/client/src/context/auth/authState.js
+++ contact-keeper/client/src/context/auth/authState.js
@@ -14,14 +14,33 @@ import {
 
 const AuthState = props => {
     const initialState = { 
+        token: localStorage.getItem('token'),
+        isAuthenticated: null,
+        loading: true ,
+        error: null,
+        user: null
     };
 
     const [state , dispatch ] = useReducer(AuthReducer , initialState); 
 
+    // Load User
+
+    // Register User
+
+    // Login User
+
+    // Logout
+
+    // Clear Errors
      
     return (
         <AuthContext.Provider 
         value={{ 
+            token: state.token,
+            isAuthenticated:  state.isAuthenticated,
+            loading:  state.loading,
+            error: state.error,
+            user: state.user
         }}>
             { props.children }
         </AuthContext.Provider>
