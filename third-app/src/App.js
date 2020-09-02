b6be258727be408b4d0bfef9d57423832ee20583
Kiril <kiril_zafirov@yahoo.com>
2020-09-02 13:01:25 +0200

    modals

--- third-app/src/App.js
+++ third-app/src/App.js
@@ -4,6 +4,11 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 import 'materialize-css/dist/css/materialize.min.css';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 import './App.css';
 
@@ -17,8 +22,14 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
+
 
     </Fragment>
   );
